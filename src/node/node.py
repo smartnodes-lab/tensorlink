af31f5046202326fe00aeca016c1ff6b71c7d205
Matthew Hawken <85070849+mattjhawken@users.noreply.github.com>
2023-07-16 16:59:03 -0400

    Some basic reformatting

--- src/node/node.py
+++ src/node/node.py
@@ -20,8 +20,11 @@ class Node(threading.Thread):
         self.max_connections = max_connections
         self.debug = debug
 
-        self.nodes = []
+        self.nodes_connected = []
+        self.nodes_lost = []
+
         self.message_count = 0
+        self.reconnect_attempts = 0
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -35,40 +38,118 @@ class Node(threading.Thread):
         if self.debug:
             print(f"debug {message}")
 
-    def connect_user(self, host: str, port: int, reconnect: bool = False) -> bool:
+    def print_connections(self) -> None:
+        print("Node connection overview:")
+        print(f"Total nodes connected: {len(self.nodes_connected)}")
+
+    def connect_node(self, host: str, port: int, reconnect: bool = False) -> bool:
         if host == self.host and port == self.port:
             self.debug_print(f"Cannot connect to self!")
-        elif any(node.host == host and node.port == port for node in self.nodes):
+        elif any(node.host == host and node.port == port for node in self.nodes_connected):
             print("Already connected with node.")
             return True
+
+        node_ids = [node.id for node in self.nodes_connected]
 
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.debug_print(f"Connecting to {host}:{port}")
             sock.connect((host, port))
 
-            # Exchange ID info betweeen nodes
+            # Exchange ID info between nodes
             sock.send((self.id + ":" + str(self.port)).encode())
             connected_node_id = sock.recv(BUFF_SIZE).decode()
 
             if self.id == connected_node_id or connected_node_id in node_ids:
-                sock.send("Already connected!".encode())
+                sock.send("Already connected to client!".encode())
                 sock.close()
                 return True
+
+            thread_client = self.create_connection(sock, connected_node_id, host, port)
+            thread_client.start()
+
+            self.nodes_connected.append(thread_client)
+            self.debug_print(f"Node connected!")
+
+            if reconnect:
+                self.debug_print(f"Reconnection ")
+                self.nodes_lost.append({
+                    "host": host, "port": port, "tries": 0
+                })
+
+            return True
 
         except Exception as e:
             self.debug_print(f"Could not connect with node: {e}")
             return False
 
-    def send_to_node(self, node: NodeConnection, data: bytes):
-        if node in self.nodes:
+    def create_connection(self, connection: socket.socket, id: str, host: str, port: int) -> NodeConnection:
+        return NodeConnection(self, connection, id, host, port)
+
+    def delete_connection(self, node: NodeConnection) -> None:
+        if node in self.nodes_connected:
+            self.debug_print(f"Disconnecting with node: {node.host}:{self.port}")
+            node.stop()
+        else:
+            self.debug_print(f"Cannot disconnect with node {node.host}:{node.port} (does not exist!)")
+
+    def stop(self) -> None:
+        self.debug_print(f"Node requested to stop.")
+        self.terminate_flag.set()
+
+    def send_to_node(self, node: NodeConnection, data: bytes) -> None:
+        if node in self.nodes_connected:
             node.send(data)
             self.message_count += 1
         else:
             self.debug_print(f"Could not connect to user.")
 
-    def send_to_nodes(self, data: bytes, exclude: List[NodeConnection]):
-        nodes = filter(lambda node: node not in exclude, self.nodes)
+    def send_to_nodes(self, data: bytes, exclude: List[NodeConnection]) -> None:
+        nodes = filter(lambda node: node not in exclude, self.nodes_connected)
 
         for node in nodes:
             self.send_to_node(node, data)
+
+    def reconnect_nodes(self) -> None:
+        for lost_node in self.nodes_lost:
+            lh = lost_node.host
+            lp = lost_node.port
+
+            found = False
+            self.debug_print(f"Attempting to reconnect with {lh}:{lp}")
+
+            for node in self.nodes_connected:
+                if node.host == lh and node.port == lp:
+                    found = True
+                    lost_node.reconnect_attempts = 0
+                    self.debug_print(f"Reconnected to node {node.host}:{node.port}")
+
+            if not found:
+                lost_node.reconnect_attempts += 1
+                self.debug_print(f"Reconnecting to node {lh}:{lp} "
+                                 f"(Attempt {lost_node.reconnect_attempts})")
+                self.connect_node(lh, lp)
+
+    def run(self) -> None:
+        while not self.terminate_flag.is_set():
+            try:
+                self.debug_print("Awaiting connections.")
+                # self.model.train_loop(1) from previous rendition of neuronn
+                connection, client_address = self.sock.accept()
+
+                self.debug_print(f"Total connections: " + str(self.nodes_connected))
+
+                if self.max_connections == 0 or len(self.nodes_connected) < self.max_connections:
+                    # Non-secured node-id exchange
+                    client_address = [1]
+
+            except:
+                pass
+
+    def node_message(self, node: NodeConnection, data):
+        self.debug_print(f"node-message::{node.id}: {data}")
+
+    def node_disconnect(self, node: NodeConnection):
+        if node in self.nodes_connected:
+            self.debug_print(f"node::disconnecting::{node.id}")
+            del self.nodes_connected[self.nodes_connected.index(node)]
