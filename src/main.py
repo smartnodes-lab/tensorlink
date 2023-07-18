2196be751eddff44047663b426ab0d42645eed4f
mattjhawken <mattjhawken@gmail.com>
2023-07-18 15:53:30 -0400

    Saving changes. Implementing AI functionality to node.

--- src/main.py
+++ src/main.py
@@ -4,14 +4,15 @@ import os
 
 # Read seed from file in dir
 def get_local_seed():
-    if not os.path.exists("SEED.txt"):
-        raise FileNotFoundError("SEED.txt not found!")
+    if not os.path.exists("seed.txt"):
+        raise FileNotFoundError("seed.txt not found!")
 
-    with open("SEED.txt", "r") as file:
+    with open("seed.txt", "r") as file:
         return file.read().strip()
 
 
 if __name__ == "__main__":
+    # Connect to the ml-net contract and await jobs?
     url = "wss://ws.test.azero.dev"
-    # seed = get_local_seed()
+    seed = get_local_seed()
     substrate = SubstrateInterface(url=url)
---
+++ src/node_connection.py
@@ -0,0 +1,190 @@
+from typing import Union, Any
+import socket
+import time
+import threading
+import json
+import zlib
+import base64
+
+
+class NodeConnection(threading.Thread):
+    """Used by the class Node and represent the TCP/IP socket connection with another node.
+
+       Both inbound (nodes that connect with the server) and outbound (nodes that are connected to) are represented by
+       this class. The class contains the client socket and hold the id information of the connecting node.
+       Communication is done by this class. When a connecting node sends a message, the message is relayed to the
+       main node (that created this NodeConnection in the first place).
+       
+       Instantiates a new NodeConnection. Do not forget to start the thread. All TCP/IP communication is handled by this 
+       connection.
+        main_node: The Node class that received a connection.
+        sock: The socket that is associated with the client connection.
+        id: The id of the connected node (at the other side of the TCP/IP connection).
+        host: The host/ip of the main node.
+        port: The port of the server of the main node."""
+
+    def __init__(self, main_node, sock: socket.socket, id: str, host: str, port: int):
+        super(NodeConnection, self).__init__()
+
+        self.host = host
+        self.port = port
+        self.main_node = main_node
+        self.sock = sock
+        self.terminate_flag = threading.Event()
+
+        # The id of the connected node
+        self.id = str(id)  # Make sure the ID is a string
+
+        # End of transmission character for the network streaming messages.
+        self.EOT_CHAR = 0x04.to_bytes(1, 'big')
+
+        # Indication that the message has been compressed
+        self.COMPR_CHAR = 0x02.to_bytes(1, 'big')
+
+        # Datastore to store additional information concerning the node.
+        self.info = {}
+
+        # Use socket timeout to determine problems with the connection
+        self.sock.settimeout(10.0)
+
+        self.main_node.debug_print(
+            f"NodeConnection: Started with client ({self.id}) '{self.host}:{self.port}'"
+        )
+
+    def compress(self, data):
+        """Compresses the data given the type. It is used to provide compression to lower the network traffic in case of
+           large data chunks. It stores the compression type inside the data, so it can be easily retrieved."""
+        self.main_node.debug_print(self.id + ":compress:input: " + str(data))
+
+        compressed = data
+
+        try:
+            compressed = base64.b64encode(zlib.compress(data, 6))
+
+        except Exception as e:
+            self.main_node.debug_print("compress: exception: " + str(e))
+
+        self.main_node.debug_print(self.id + ":compress:b64encode:" + str(compressed))
+        self.main_node.debug_print(
+            self.id + ":compress:compression:" + str(int(10000 * len(compressed) / len(data)) / 100) + "%")
+
+        return compressed
+
+    def decompress(self, compressed):
+        """Decompresses the data given the type. It is used to provide compression to lower the network traffic in case of
+           large data chunks."""
+        self.main_node.debug_print(self.id + ":decompress:input: " + str(compressed))
+        decompressed = base64.b64decode(compressed)
+        self.main_node.debug_print(self.id + ":decompress:b64decode: " + str(compressed))
+
+        try:
+            decompressed = zlib.decompress(compressed)
+        except Exception as e:
+            print("Exception: " + str(e))
+
+        self.main_node.debug_print(self.id + ":decompress:result: " + str(decompressed))
+
+        return decompressed
+
+    def send(self, data: bytes, compression: bool = False):
+        """Send the data to the connected node. The data can be pure text (str), dict object (send as json) and bytes object.
+           When sending bytes object, it will be using standard socket communication. A end of transmission character 0x04 
+           utf-8/ascii will be used to decode the packets ate the other node. When the socket is corrupted the node connection
+           is closed. Compression can be enabled by using zlib, bzip2 or lzma. When enabled the data is compressed and send to
+           the client. This could reduce the network bandwith when sending large data chunks.
+           """
+        try:
+            if compression:
+                data = self.compress(data)
+                if data is not None:
+                    self.sock.sendall(data + self.COMPR_CHAR + self.EOT_CHAR)
+            else:
+                self.sock.sendall(data + self.EOT_CHAR)
+
+        except Exception as e:  # Fixed issue #19: When sending is corrupted, close the connection
+            self.main_node.debug_print("nodeconnection send: Error sending data to node: " + str(e))
+            self.stop()  # Stopping node due to failure
+
+    def stop(self) -> None:
+        """Terminates the connection and the thread is stopped.
+        Please make sure you join the thread."""
+        self.terminate_flag.set()
+
+    def parse_packet(self, packet) -> Union[str, dict, bytes]:
+        """Parse the packet and determines whether it has been send in str, json or byte format. It returns
+           the according data."""
+        if packet.find(self.COMPR_CHAR) == len(packet) - 1:  # Check if packet was compressed
+            packet = self.decompress(packet[0:-1])
+
+        try:
+            packet_decoded = packet.decode('utf-8')
+
+            try:
+
+                return json.loads(packet_decoded)
+
+            except json.decoder.JSONDecodeError:
+                return packet_decoded
+
+        except UnicodeDecodeError:
+            return packet
+
+    def run(self):
+        """The main loop of the thread to handle the connection with the node. Within the
+           main loop the thread waits to receive data from the node. If data is received 
+           the method node_message will be invoked of the main node to be processed."""
+        buffer = b''  # Hold the stream that comes in!
+
+        while not self.terminate_flag.is_set():
+            chunk = b''
+
+            try:
+                chunk = self.sock.recv(4096)
+
+            except socket.timeout:
+                self.main_node.debug_print("NodeConnection: timeout")
+
+            except Exception as e:
+                self.terminate_flag.set()  # Exception occurred terminating the connection
+                self.main_node.debug_print('Unexpected error')
+                self.main_node.debug_print(e)
+
+            # BUG: possible buffer overflow when no EOT_CHAR is found => Fix by max buffer count or so?
+            if chunk != b'':
+                buffer += chunk
+                eot_pos = buffer.find(self.EOT_CHAR)
+
+                while eot_pos > 0:
+                    packet = buffer[:eot_pos]
+                    buffer = buffer[eot_pos + 1:]
+
+                    self.main_node.message_count_recv += 1
+                    self.main_node.node_message(self, self.parse_packet(packet))
+
+                    eot_pos = buffer.find(self.EOT_CHAR)
+
+            time.sleep(0.01)
+
+        # IDEA: Invoke (event) a method in main_node so the user
+        # is able to send a bye message to the node before it is closed?
+        self.sock.settimeout(None)
+        self.sock.close()
+        # Fixed issue #19: Send to main_node when a node is disconnected.
+        # We do not know whether it is inbound or outbound.
+        self.main_node.node_disconnected(self)
+        self.main_node.debug_print("NodeConnection: Stopped")
+
+    def set_info(self, key: str, value: Any) -> Any:
+        self.info[key] = value
+
+    def get_info(self, key: str) -> Any:
+        return self.info[key]
+
+    def __str__(self) -> str:
+        return 'NodeConnection: {}:{} <-> {}:{} ({})'.format(
+            self.main_node.host, self.main_node.port, self.host, self.port,
+            self.id)
+
+    def __repr__(self) -> str:
+        return '<NodeConnection: Node {}:{} <-> Connection {}:{}>'.format(
+            self.main_node.host, self.main_node.port, self.host, self.port)
